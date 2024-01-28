bb6c8106c378458ca837a0d8b968969fcdca36a1
Freddie Lane <frederick2.lane@live.uwe.ac.uk>
2024-01-28 14:38:04 +0000

    Restart menu

--- Project Giggletouch/Assets/scripts/MenuButtons.cs
+++ Project Giggletouch/Assets/scripts/MenuButtons.cs
@@ -1,32 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
-    public GameObject MainMenu;
-    public GameObject CreditsMenu;
 
-    private void Start()
-    {
-        MainMenuButton();
-    }
 
-    public void PlayNowButton()
-    {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("level");
-    }
 
-    public void CreditsButton()
+    public void PlayButton()
     {
-        MainMenu.SetActive(false);
-        CreditsMenu.SetActive(true);
-    }
-
-    public void MainMenuButton()
-    {
-        MainMenu.SetActive(true);
-        CreditsMenu.SetActive(false);
+        SceneManager.LoadScene("level");
     }
 
     public void QuitButton()
